a20f59fae641c2487edda3f86bdd804ca67e7482
c0nD <colintiller21@gmail.com>
2022-12-02 19:26:00 -0500

    now defers on killed commands

--- bot.py
+++ bot.py
@@ -34,9 +34,9 @@ guilds = {
 }
 
 ping_roles = {
-    "aod": 1040927294123937852,
-    "tla": 1040927394288115753,
-    "rvd": 1040927439343341620
+    "aod": 1047787785895038986,
+    "tla": 1047787857357590538,
+    "rvd": 1042512104059568138
 }
 
 def run_bot():
@@ -80,7 +80,6 @@ def run_bot():
     @bot.tree.command(name="admin_hit", description="HIT THE BOSS TO FIX THE HP -- WILL NOT REGISTER AS A HIT.")
     @app_commands.describe(damage="Enter the exact amount to deal to the boss.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def admin_hit(interaction: discord.Interaction, damage: str):
         await interaction.response.send_message("Attempting to hit...")  # Deferring so I can followup later
         res = bool(bot.boss_dict.get(interaction.channel_id))
@@ -126,21 +125,22 @@ def run_bot():
 
     @bot.tree.command(name="admin_kill", description="KILL THE BOSS TO FIX THE LEVEL -- WILL NOT REGISTER AS A HIT.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def admin_kill(interaction: discord.Interaction):
+        await interaction.response.send_message("Attempting to kill...")  # Deferring so I can followup later
         res = bool(bot.boss_dict.get(interaction.channel_id))
         if not res:
-            await interaction.response.send_message(
+            await interaction.followup.send(
                 "A boss has not been set up in this channel. If this is a mistake, please contact c0nD ")
             return
         if str(interaction.channel.name).lower() in valid_channels:
             curr_boss = bot.boss_dict[interaction.channel_id]
             curr_boss.admin_kill()
             allowed_mentions = discord.AllowedMentions(everyone=True)
-            await interaction.response.send_message(f"**_ADMIN_ has killed the boss. New Boss:**",
+            await interaction.followup.send(f"**_ADMIN_ has killed the boss. New Boss:**",
                                                     allowed_mentions=allowed_mentions)
             embed = get_hp_embed(interaction, curr_boss)
             await interaction.followup.send(embed=embed)
+        await interaction.delete_original_response()
 
     @bot.tree.command(name="admin_revive", description="REVIVE THE BOSS TO FIX THE LEVEL -- WILL NOT REGISTER AS A HIT")
     @app_commands.guild_only()
@@ -166,7 +166,6 @@ def run_bot():
     @bot.tree.command(name="create_boss", description="Add a boss to this channel.")
     @app_commands.describe(guild="Enter the guild this boss belongs to (ie. Onion, Spring, etc).")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def create_boss(interaction: discord.Interaction, guild: str):
         guild = guild.lower()
         if guild not in guilds:
@@ -194,7 +193,6 @@ def run_bot():
     @bot.tree.command(name="delete_boss", description="Delete the boss out of the current channel."
                                                       " Use carefully.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def delete_boss(interaction: discord.Interaction):
         if str(interaction.channel.name).lower() in valid_channels:
             try:
@@ -211,7 +209,6 @@ def run_bot():
                            level="Enter the level of the boss to be inserted.",
                            health="Enter the health of the boss to be inserted.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def insert_boss(interaction: discord.Interaction, guild: str,
                           level: str, health: str):
         if str(interaction.channel.name).lower() in valid_channels:
@@ -254,7 +251,6 @@ def run_bot():
     @bot.tree.command(name="hit", description="Uses 1 ticket to hit the boss.")
     @app_commands.describe(damage="Enter the exact amount of damage dealt to the boss.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def hit(interaction: discord.Interaction, damage: str):
         await interaction.response.send_message("Attempting to hit...")  # Deferring so I can followup later
         res = bool(bot.boss_dict.get(interaction.channel_id))
@@ -306,7 +302,6 @@ def run_bot():
     @bot.tree.command(name="resume_hit", description="Hit the boss *without* using a ticket (aka Continued hit).")
     @app_commands.describe(damage="Enter the exact amount of damage dealt to the boss.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def resume_hit(interaction: discord.Interaction, damage: str):
         await interaction.response.send_message("Attempting to hit...")  # Deferring so I can followup later
         res = bool(bot.boss_dict.get(interaction.channel_id))
@@ -356,11 +351,11 @@ def run_bot():
 
     @bot.tree.command(name="killed", description="Uses a ticket and kills the boss.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def killed(interaction: discord.Interaction):
+        await interaction.response.send_message("Attempting to kill...")  # Deferring so I can followup later
         res = bool(bot.boss_dict.get(interaction.channel_id))
         if not res:
-            await interaction.response.send_message(
+            await interaction.followup.send(
                 "A boss has not been set up in this channel. Please contact staff if you think this is a mistake.")
             return
         if str(interaction.channel.name).lower() in valid_channels:
@@ -369,19 +364,20 @@ def run_bot():
             curr_boss.killed()
             allowed_mentions = discord.AllowedMentions(everyone=True)
             ping = discord.utils.get(interaction.guild.roles, id=ping_roles[interaction.channel.name])
-            await interaction.response.send_message(f"**{ping.mention} has been swept. New Boss:**",
+            await interaction.followup.send(f"**{ping.mention} has been swept. New Boss:**",
                                                     allowed_mentions=allowed_mentions)
             embed = get_hp_embed(interaction, curr_boss)
             await interaction.followup.send(embed=embed)
+        await interaction.delete_original_response()
 
     # Killed command for when you don't want it to subtract a ticket
     @bot.tree.command(name="bonus_kill", description="Kill the boss *without* using a ticket (aka solo'd).")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def bonus_kill(interaction: discord.Interaction):
+        await interaction.response.send_message("Attempting to kill...")  # Deferring so I can followup later
         res = bool(bot.boss_dict.get(interaction.channel_id))
         if not res:
-            await interaction.response.send_message(
+            await interaction.followup.send(
                 "A boss has not been set up in this channel. Please contact staff if you think this is a mistake.")
             return
         if str(interaction.channel.name).lower() in valid_channels:
@@ -390,14 +386,14 @@ def run_bot():
             curr_boss.killed()
             allowed_mentions = discord.AllowedMentions(everyone=True)
             ping = discord.utils.get(interaction.guild.roles, id=ping_roles[interaction.channel.name])
-            await interaction.response.send_message(f"**{ping.mention} has been swept. New Boss:**",
+            await interaction.followup.send(f"**{ping.mention} has been swept. New Boss:**",
                                                     allowed_mentions=allowed_mentions)
             embed = get_hp_embed(interaction, curr_boss)
             await interaction.followup.send(embed=embed)
+        await interaction.delete_original_response()
 
     @bot.tree.command(name="hp", description="Check the HP of the boss.")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def hp(interaction: discord.Interaction):
         res = bool(bot.boss_dict.get(interaction.channel_id))
         if not res:
@@ -411,7 +407,6 @@ def run_bot():
 
     @bot.tree.command(name="boss_help", description="Help understand how to use Fluffy!")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def boss_help(interaction: discord.Interaction):
         embed = discord.Embed(title="Documentation",
                               description="Please click on the link above to view the documentation for all"
@@ -465,7 +460,6 @@ def run_bot():
 
     @bot.tree.command(name="send_backup_csv", description="Loads the current data.json into a csv to be exported")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def send_backup_csv(interaction: discord.Interaction):
         await interaction.response.send_message("Converting data to csv file...")
 
@@ -478,7 +472,6 @@ def run_bot():
 
     @bot.tree.command(name="send_csv", description="Loads the current data.json into a csv to be exported")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def send_csv(interaction: discord.Interaction):
         await interaction.response.send_message("Converting data to csv file...")
 
@@ -492,7 +485,6 @@ def run_bot():
     @bot.tree.command(name="load_json", description="Loads the current data.json into the boss_dict (USED TO RESTORE"
                                                     " FROM A BACKUP)")
     @app_commands.guild_only()
-    @app_commands.guilds(guild_id)
     async def load_json(interaction: discord.Interaction):
         await interaction.response.send_message("Sending json file to dictionary...")
         with open("data.json") as outfile:
