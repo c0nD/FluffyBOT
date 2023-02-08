680155c7bcd1f332c67493139e4d10153ededbc4
Colin <90937263+c0nD@users.noreply.github.com>
2023-02-07 21:21:17 -0500

    updated errors

--- final_vars.py
+++ final_vars.py
@@ -4,6 +4,7 @@ guild_id = int(linecache.getline('guild_id.txt', 1))
 admin_roles = []
 valid_channels = ['avatar', 'living_abyss', 'dragon']
 split_threshold = 3
+max_queue_length = 2
 guilds = {
     "toasted": None,
     "pearl": None,
@@ -68,26 +69,28 @@ split_exempt = ["onion", "burnt", "toasted", "royal", "pearl", "onion_sandbox", 
                 "pearl_sandbox", "burnt_sandbox", "royal_sandbox", "spring_sandbox", "fall_sandbox", "dev"]
 
 # ERRORS MADE TO BE CONSTANTS
-INVALID_INT_ERR = '''**Please double check that you input a full integer value, correct number for damage
+INVALID_INT_ERR = '''**ERROR: Please double check that you input a full integer value, correct number for damage
 (will not accept comma separated numbers or numbers ending with 'm' (123.4m)). 
 If you want to kill the boss, please use `/admin_kill` instead. If there is some other error: please contact a developer.**'''
 
-INSERT_HIT_ERR = '''**Please double check that you input the exact, correct number for damage
+INSERT_HIT_ERR = '''**ERROR: Please double check that you input the exact, correct number for damage
 (will not accept comma separated numbers or numbers ending with 'm' (123.4m). 
 If you killed the boss please use the `/killed` command before calling `/hit` if you just swept this boss.
 If neither of these are the case, please contact your staff to get them to reset the boss at it's current level and hp.**'''
 
-BOSS_SETUP_ERR = '''**A boss has not been set up in this channel. Check /help for more information.
+BOSS_SETUP_ERR = '''**ERROR: A boss has not been set up in this channel. Check /help for more information.
 Please contact staff/a developer if you think this is a mistake.**'''
 
-INVALID_GUILD_ERR = '''**Invalid guild entered. Check /help for more information.
+INVALID_GUILD_ERR = '''**ERROR: Invalid guild entered. Check /help for more information.
 Please contact staff/a developer if you think this is a mistake.**'''
 
-INVALID_BOSS_ERR = '''**Cannot create two bosses at once, or no boss exists in this channel. 
+INVALID_BOSS_ERR = '''**ERROR: Cannot create two bosses at once, or no boss exists in this channel. 
 If you want to reset the boss, please call `/delete_boss` first. Otherwise, use `/create_boss` to make a new boss.**'''
 
-INVALID_CHANNEL_ERR = "**Cannot create boss in this channel. Please try this command again in a valid channel.**"
+INVALID_CHANNEL_ERR = "**ERROR: Cannot create boss in this channel. Please try this command again in a valid channel.**"
 
-INVALID_HP_ERR = "**HP was set higher than boss level allows for. Please try again with valid HP.**"
+INVALID_HP_ERR = "**ERROR: HP was set higher than boss level allows for. Please try again with valid HP.**"
 
-INVALID_UNDO_ERR = "**You can only undo hits on the current level, or kills on the last level if no other commands have been used after that.**"
+INVALID_UNDO_ERR = "**ERROR: You can only undo hits on the current level, or kills on the last level if no other commands have been used after that.**"
+
+INVALID_PARAM_ERR = "**ERROR: The parameter that has been entered is incorrect. Please either check the command description or /help for more information.**"
