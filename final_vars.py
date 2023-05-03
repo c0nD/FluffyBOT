f2197be79f81b21307e7f94c72d8c80ddded7f0d
c0nD <colintiller21@gmail.com>
2023-05-03 14:18:13 +0000

    added dmg intervals

--- final_vars.py
+++ final_vars.py
@@ -65,18 +65,50 @@ sweeper_requirements = {
     }
 }
 
+damage_intervals = {
+    "onion": {
+        "avatar": {"low": 100_000_000, "high": 150_000_000},
+        "living_abyss": {"low": 200_000_000, "high": 350_000_000},
+        "dragon": {"low": 200_000_000, "high": 350_000_000}
+    },
+    "fall": {
+        "avatar": {"low": 100_000_000, "high": 150_000_000},
+        "living_abyss": {"low": 190_000_000, "high": 300_000_000},
+        "dragon": {"low": 150_000_000, "high": 290_000_000}
+    },
+    "spring": {
+        "avatar": {"low": 100_000_000, "high": 150_000_000},
+        "living_abyss": {"low": 151_000_000, "high": 262_000_000},
+        "dragon": {"low": 140_000_000, "high": 250_000_000}
+    },
+    "burnt": {
+        "avatar": {"low": 100_000_000, "high": 150_000_000},
+        "living_abyss": {"low": 160_000_000, "high": 230_000_000},
+        "dragon": {"low": 170_000_000, "high": 240_000_000}
+    },
+    "royal": {
+        "avatar": {"low": 100_000_000, "high": 150_000_000},
+        "living_abyss": {"low": 100_000_000, "high": 300_000_000},
+        "dragon": {"low": 120_000_000, "high": 280_000_000}
+    },
+    "other": {
+        "avatar": {"low": 100_000_000, "high": 150_000_000},
+        "living_abyss": {"low": 200_000_000, "high": 350_000_000},
+        "dragon": {"low": 200_000_000, "high": 350_000_000}
+    }
+}
+
 split_exempt = ["onion", "spring", "fall", "burnt", "toasted", "royal", "pearl", "onion_sandbox", "toasted_sandbox", 
                 "pearl_sandbox", "burnt_sandbox", "royal_sandbox", "spring_sandbox", "fall_sandbox", "dev"]
 
 # ERRORS MADE TO BE CONSTANTS
-INVALID_INT_ERR = '''**ERROR: Please double check that you input a full integer value, correct number for damage
-(will not accept comma separated numbers or numbers ending with 'm' (123.4m)). 
-If you want to kill the boss, please use `/admin_kill` instead. If there is some other error: please contact a developer.**'''
+INVALID_INT_ERR = '''**ERROR: Please double check that you input a full integer value, correct number for level and hp
+(will not accept comma separated numbers). 
+If there is some other error: please contact a developer.**'''
 
-INSERT_HIT_ERR = '''**ERROR: Please double check that you input the exact, correct number for damage
-(will not accept comma separated numbers or numbers ending with 'm' (123.4m). 
-If you killed the boss please use the `/killed` command before calling `/hit` if you just swept this boss.
-If neither of these are the case, please contact your staff to get them to reset the boss at it's current level and hp.**'''
+INSERT_HIT_ERR = '''**ERROR: Please double check that you input the exact, correct number for level and hp
+(will not accept comma separated numbers). 
+If there is some other error, please contact your staff to get them to reset the boss at it's current level and hp.**'''
 
 BOSS_SETUP_ERR = '''**ERROR: A boss has not been set up in this channel. Check /help for more information.
 Please contact staff/a developer if you think this is a mistake.**'''
@@ -91,6 +123,6 @@ INVALID_CHANNEL_ERR = "**ERROR: Cannot create boss in this channel. Please try t
 
 INVALID_HP_ERR = "**ERROR: HP was set higher than boss level allows for. Please try again with valid HP.**"
 
-INVALID_UNDO_ERR = "**ERROR: You can only undo hits on the current level, or kills on the last level if no other commands have been used after that.**"
+INVALID_UNDO_ERR = "**ERROR: You can only undo the last command if you ran it.**"
 
 INVALID_PARAM_ERR = "**ERROR: The parameter that has been entered is incorrect. Please either check the command description or /help for more information.**"
